afb8f8607d073dfafa3c6d7fd3ed2c5a3dd1d161
jaseeerr <jaseerhasbulla01@gmail.com>
2023-06-21 13:47:55 +0530

    v3

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -2,11 +2,12 @@ import React from "react"
 import { useFormik } from "formik"
 import MyAxiosInstance from '../../utils/axios';
 import toast, {Toaster} from 'react-hot-toast'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const Signup = ()=>{
 
 
     const axiosInstance = MyAxiosInstance();
+    const goto = useNavigate() 
 
 // formik starts
 const formik = useFormik({
@@ -62,6 +63,12 @@ const formik = useFormik({
                 if(response.data.success)
                 {
                     toast.success("Signup Complete")
+                    setTimeout(()=>{
+                      toast.dismiss()
+                      goto("/login-user")
+
+                    },1000)
+                  
                 }
                 else if(response.data.exuser)
                 {
@@ -116,7 +123,7 @@ const formik = useFormik({
                       <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Phone</label>
                       <input
                          {...formik.getFieldProps('phone')}
-                      type="text" name="phone" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required/>
+                      type="text" name="phone" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Phone" required/>
                   {formik.errors.phone ? (
                 <div style={{color:"red"}}>{formik.errors.phone}</div>
               ) : null}
--- src/config/URLS.js
+++ src/config/URLS.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "http://localhost:3000/"
+export const BASE_URL = "http://16.170.232.111/"
